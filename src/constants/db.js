084ae5ecdd9a4bb005c7b3da404aa93cd2cc64b2
Asliddin <nematullayevasliddin11@gmail.com>
2025-05-09 19:38:39 +0500

    first commit

---
+++ src/constants/db.js
@@ -0,0 +1,67 @@
+export function getData() {
+	return [
+		{
+			title: 'JavaScript',
+			price: 17.99,
+			Image:
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo01pCyzWVJWIAxq9EjKgNoL7zftvlDbOmru0H2&w=1920&q=75&dpl=dpl_AyWjnwefx2RFAeEprCzJhU8VkTuz',
+			id: 1,
+		},
+		{
+			title: 'ReactJS',
+			price: 15,
+			Image:
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0iUp6fYhocXUlvFEwuZWOT81zHSksDrGxN4f9&w=1920&q=75&dpl=dpl_AyWjnwefx2RFAeEprCzJhU8VkTuz',
+			id: 2,
+		},
+		{
+			title: 'VueJS',
+			price: 13.5,
+			Image:
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0pJtmiWIV0jo2XA16Ydy4rSfwLIiRxv98gOuk&w=1920&q=75&dpl=dpl_AyWjnwefx2RFAeEprCzJhU8VkTuz',
+			id: 3,
+		},
+		{
+			title: 'NodeJS',
+			price: 13.99,
+			Image:
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0Dt6eWDpQTl9a4CLgOmnEFkfdiNbwx068Xc3o&w=1920&q=75&dpl=dpl_AyWjnwefx2RFAeEprCzJhU8VkTuz',
+			id: 4,
+		},
+		{
+			title: 'NestJS',
+			price: 12.5,
+			Image:
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0WO04xMuv70rfVnCNDiebGQXS2lh4oIUJ8tdw&w=1920&q=75&dpl=dpl_AyWjnwefx2RFAeEprCzJhU8VkTuz',
+			id: 5,
+		},
+		{
+			title: 'NextJS',
+			price: 12.99,
+			Image:
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo041nrlbd9utCZemPzyoJSOTAbr18IvF0MYQ6H&w=1920&q=75&dpl=dpl_AyWjnwefx2RFAeEprCzJhU8VkTuz',
+			id: 6,
+		},
+		{
+			title: 'TypeScript',
+			price: 22.99,
+			Image:
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0E5lF6amAU8kioLXqN3YRt4exAMZ2yFscwGIl&w=1920&q=75&dpl=dpl_AyWjnwefx2RFAeEprCzJhU8VkTuz',
+			id: 7,
+		},
+		{
+			title: 'Foundation',
+			price: 14.55,
+			Image:
+				'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo00Gmjkh79cpx21MsQhvWPLIgeAHGwlaumbU8j&w=1920&q=75&dpl=dpl_AyWjnwefx2RFAeEprCzJhU8VkTuz',
+			id: 8,
+		},
+		{
+			title: 'TensorflowJS',
+			price: 28.99,
+			Image:
+				'https://sammi.ac/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FtIMgibghQveiDeJeXDPI&w=3840&q=75',
+			id: 9,
+		},
+	]
+}
